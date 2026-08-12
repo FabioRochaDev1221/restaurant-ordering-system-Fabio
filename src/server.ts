93cd84173d677c4c32d4a92865bb200376f8d1ec
FabioRochaDev1221 <fabio.idrocha1221@gmail.com>
2026-08-11 21:41:05 -0300

    adiciona routes

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,30 @@ import { version } from "node:os";
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    const response = {
-        message: "API restaurante",
-        version: "1.0.0",
-        outros: "XPTO"
-    };
+const routes = {
+    "/":{
+            message: "Pagina restaurante",
+    },
 
+    "/categorias": {
+        message: "Lista de categorias",
+    },
+
+    "/produtos": {
+        message: "Lista de produtos",
+    },
+};
+
+const server = http.createServer((req, res) => {
+    const response = routes[req.url as keyof typeof routes];
+
+    if (!response){
+            res.writeHead(404, {
+            "content-type": "application/json",
+        });
+
+        return res.end(JSON.stringify({message: "Rota Não encontrada"}));
+    }
     res.writeHead(200, {
         "content-type": "application/json",
     });
